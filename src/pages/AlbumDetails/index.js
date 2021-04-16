0372879f7f4f4e4a0f9729e6e7e0e82ee84798c0
Guus Dijkhuis <guus.dijkhuis@gmail.com>
2021-04-16 18:28:00 +0200

    fix

--- src/pages/AlbumDetails/index.js
+++ src/pages/AlbumDetails/index.js
@@ -3,8 +3,6 @@ import Header from '../../components/organisms/Header';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Details from '../../components/templates/Details';
 const page = (req, res) => {
-	
-
 	return (
 		<div>
 			<CssBaseline />
